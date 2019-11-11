2b3ed012be450ea9ce2b32ada1b5d4ca7a6fe95d
ezakharov <NeNjktrf1234566>
2019-11-11 12:35:05 +0300

    Add terraform modules

---
+++ aws/terraform_2/playbook/vpc/vpc.tf
@@ -0,0 +1,26 @@
+variable "aws_access_key" {
+  default = "AKIATTBUSM2QZYRIY3U4"
+}
+variable "aws_secret_key" {
+  default = "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"
+}
+provider "aws" {
+  region  = "us-east-2"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+}
+module "vpc" {
+  source                              = ".//../../modules/vpc"
+  name                                = "TEST-VPC"
+  environment                         = "TEST"
+  instance_tenancy                    = "dedicated"
+  enable_dns_support                  = "true"
+  enable_dns_hostnames                = "true"
+  assign_generated_ipv6_cidr_block    = "false"
+  enable_classiclink                  = "false"
+  vpc_cidr                            = "172.30.0.0/16"
+  private_subnet_cidrs                = ["172.30.60.0/20"]
+  public_subnet_cidrs                 = ["172.30.80.0/20"]
+  availability_zones                  = ["us-east-2a", "us-east-2b"]
+  allowed_ports                       = ["80", "443"]
+}
