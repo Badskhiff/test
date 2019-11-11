105b7cad350409e50ba98d3b25f6302c74e6acfe
Badskhiff <badskhiff@gmail.com>
2019-11-12 00:11:57 +0300

    Update terraform

--- aws/terraform/playbook/alb/alb.tf
+++ aws/terraform/playbook/alb/alb.tf
@@ -1,9 +1,4 @@
-variable "aws_access_key" {
-  default = "AKIATTBUSM2QZYRIY3U4"
-}
-variable "aws_secret_key" {
-  default = "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"
-}
+
 provider "aws" {
   region  = "us-east-2"
   #shared_credentials_file = "${pathexpand("~/.aws/credentials")}"
