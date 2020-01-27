6e80573370d294ac8cbb57a6d088715cdf498b5a
ezakharov <NeNjktrf1234566>
2020-01-27 19:18:00 +0300

    Add modules

---
+++ aws/nginx/terraform/playbook/alb/alb.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region  = "us-east-2"
+  shared_credentials_file = "${pathexpand("~/.aws/credentials")}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+}
+module "alb" {
+  source                  = ".//../../modules/alb"
+  name                    = "App-Load-Balancer"
+  region                  = "us-east-2"
+  environment             = "TEST"
+
+  load_balancer_type          = "application"
+  security_groups             = ["TEST SECURITY"]
+  subnets                     = data.aws_subnet_ids.private.ids
+  vpc_id                      = data.aws_vpcs.vpc_id.ids
+  enable_deletion_protection  = false
+
+  backend_protocol    = "HTTP"
+  alb_protocols       = "HTTP"
+
+}
+data "aws_vpcs" "vpc_id" {
+  tags = {
+    Name = "TEST VPC"
+  }
+}
+data "aws_subnet_ids" "private" {
+  vpc_id = data.aws_vpcs.vpc_id.ids
+
+  tags = {
+    Tier = "Private"
+  }
+}
