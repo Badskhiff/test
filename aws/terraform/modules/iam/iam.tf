31760b9c3ad8255d109c71014d936c13a0295e19
ezakharov <NeNjktrf1234566>
2020-01-28 20:05:03 +0300

    Add modules

--- aws/terraform/modules/iam/iam.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_iam_role" "sns_role" {
-  name               = "sns_role"
-  assume_role_policy = file("${path.module}/sns_polisy.json")
-}
--- aws/terraform/playbook/iam/iam.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region  = "us-east-2"
-  shared_credentials_file = "${pathexpand("~/.aws/credentials")}"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-}
-module "sns" {
-  source                              = ".//../../modules/iam"
-}
